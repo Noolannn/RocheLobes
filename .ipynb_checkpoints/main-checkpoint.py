d12a261a8290b18d204fd0c7a8f4bc131ef5efe4
Raiigo <nolann.juet@gmail.com>
2026-02-05 16:40:49 +0100

    Added Newton method to find Lagrange points

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,377 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import measure
+
+q = 0.25
+
+# Physical constant
+G = 1
+
+# Simulation parameters
+delta   = 0.0005
+epsilon = 1e-7
+size    = 2
+
+# Physical parameters
+# M=1, a=1
+M = 1
+a = 1
+Omega = np.sqrt(G*M/a**3)
+
+def roche_potential(X, Y, q):
+    M1 = M/(1 + q)
+    M2 = M - M1
+    a2 = a/(1+q)
+    a1 = a - a2
+    a1 = a1
+    a2 = -a2
+    R = np.sqrt(X**2 + Y**2)
+    R1 = np.sqrt((X - a1)**2 + Y**2)
+    R2 = np.sqrt((X - a2)**2 + Y**2)
+    R1_eff = np.where(R1 < epsilon, epsilon, R1)
+    R2_eff = np.where(R2 < epsilon, epsilon, R2)
+    # Adds epsilon to avoid the singularity at the star position
+    Phi1 = - (G * M1)/R1_eff
+    Phi2 = - (G * M2)/R2_eff
+    PhiC = (1/2) * (Omega**2) * (R**2)
+    PhiRoche = Phi1 + Phi2 - PhiC
+    return PhiRoche
+
+def roche_potential_3D(X, Y, Z, q):
+    M1 = M/(1 + q)
+    M2 = M - M1
+    a2 = a/(1+q)
+    a1 = a - a2
+    a1 = a1
+    a2 = -a2
+    R = np.sqrt(X**2 + Y**2)
+    R1 = np.sqrt((X - a1)**2 + Y**2 + Z**2)
+    R2 = np.sqrt((X - a2)**2 + Y**2 + Z**2)
+    R1_eff = np.where(R1 < epsilon, epsilon, R1)
+    R2_eff = np.where(R2 < epsilon, epsilon, R2)
+    # Adds epsilon to avoid the singularity at the star position
+    Phi1 = - (G * M1)/R1_eff
+    Phi2 = - (G * M2)/R2_eff
+    PhiC = (1/2) * (Omega**2) * (R**2)
+    PhiRoche = Phi1 + Phi2 - PhiC
+    return PhiRoche
+
+
+mu=q/(1+q)
+def sgn(x):
+    return np.where(x>0,1.0, np.where(x<0,-1.0,0.0))
+def f(x, mu=mu):
+    return (x-(1-mu)/((x-mu)**2)*sgn(x-mu)-mu/((x-mu+1)**2)*sgn(x-mu+1))
+xmin, xmax = -2.0, 2.0
+eps = 1e-6
+xs1 = np.linspace(xmin, mu-1-eps, 4000)
+xs2 = np.linspace(mu-1+eps, mu-eps, 4000)
+xs3 = np.linspace(mu+eps, xmax, 4000)
+def bisect_root(func, a, b, err=1e-8):
+    fa, fb = func(a), func(b)
+    if not np.isfinite(fa) or not np.isfinite(fb) or fa*fb > 0:
+        return None
+    while abs(a-b)>err:
+        m = 0.5*(a+b)
+        fm = func(m)
+        if fa*fm <= 0:
+            b, fb = m, fm
+        else:
+            a, fa = m, fm
+    return 0.5*(a+b)
+
+def keep_smallest_component(faces):
+    # faces: (M,3) triangles index into verts
+    M = faces.shape[0]
+
+    # 建立“共享边 -> 相邻三角形”的图
+    edge2tris = {}
+    for t, (a,b,c) in enumerate(faces):
+        edges = [(a,b),(b,c),(c,a)]
+        for u,v in edges:
+            if u > v: u, v = v, u
+            edge2tris.setdefault((u,v), []).append(t)
+
+    adj = [[] for _ in range(M)]
+    for tris in edge2tris.values():
+        if len(tris) == 2:
+            t1, t2 = tris
+            adj[t1].append(t2)
+            adj[t2].append(t1)
+
+    # BFS/DFS 找连通分量
+    comp_id = -np.ones(M, dtype=int)
+    comp_sizes = []
+    cid = 0
+    for i in range(M):
+        if comp_id[i] != -1: 
+            continue
+        stack = [i]
+        comp_id[i] = cid
+        size = 0
+        while stack:
+            t = stack.pop()
+            size += 1
+            for nb in adj[t]:
+                if comp_id[nb] == -1:
+                    comp_id[nb] = cid
+                    stack.append(nb)
+        comp_sizes.append(size)
+        cid += 1
+
+    # 选最小的连通分量
+    smallest = int(np.argmin(comp_sizes))
+    keep = (comp_id == smallest)
+    return faces[keep]
+
+def roche_lobe_plot(q):
+    mu=q/(1+q)
+    print("mu:", mu)
+    x_L1_q = bisect_root(lambda x: f(x, mu), mu-1+1e-6, mu-1e-6)
+    print("xL1:", x_L1_q)
+    
+    
+    x5, y5, z5 = 2* np.mgrid[-1:1:201j, -1:1:201j, -1:1:201j]
+    vol = roche_potential_3D(x5, y5, z5, q)
+
+    dx5 = x5[1,0,0] - x5[0,0,0]
+    dy5 = y5[0,1,0] - y5[0,0,0]
+    dz5 = z5[0,0,1] - z5[0,0,0]
+
+    iso_val = roche_potential_3D(x_L1_q, 0, 0, q)
+    verts, faces, normals, values = measure.marching_cubes(
+        vol, level=iso_val, spacing = (dx5, dy5, dz5)
+    )
+
+    verts_phys = verts.copy()
+    verts_phys[:, 0] += x5.min()
+    verts_phys[:, 1] += y5.min()
+    verts_phys[:, 2] += z5.min()
+
+    fig5 = plt.figure(figsize=(10, 6), dpi=300)
+    ax5 = fig5.add_subplot(111, projection='3d')
+
+    faces_lobe = keep_smallest_component(faces)
+
+    ax5.plot_trisurf(
+        verts_phys[:, 0], verts_phys[:, 1], verts_phys[:, 2],
+        triangles=faces_lobe,
+        cmap='Spectral',
+        linewidth=0.2
+    )
+    ax5.set_box_aspect((2, 1, 1)) 
+    ax5.set_xlabel('x')
+    ax5.set_ylabel('y')
+    ax5.set_zlabel('z')
+    ax5.set_xlim(-1.2,1.2)
+    ax5.set_ylim(-0.5,0.5)
+    ax5.set_zlim(-0.5,0.5)
+    ax5.set_title(rf"Roche lobe ($q={q:.3f}$)")
+    ax5.view_init(elev=30, azim=-90)
+    plt.show()
+
+if __name__ == "__main__":
+    a2 = a/(1+q)
+    a1 = a - a2
+    a1 = a1
+    a2 = -a2
+    x = np.arange(-size, size, delta)
+    y = np.arange(-size, size, delta)
+    X, Y = np.meshgrid(x, y)
+    Z = roche_potential(X, Y, q)
+
+    fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
+    ax.set_aspect('equal')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_frame_on(False)
+    zmin, zmax = np.percentile(Z, [30, 100])
+    levels = np.linspace(zmin, zmax, 30)
+    print(Z.min())
+    print(Z.max())
+    #levels = np.linspace(zmin, zmax, 100)
+    #ax.scatter(0, 0, s=60, marker=".")
+    #ax.scatter(a1, 0, s=60, marker=(5, 1))
+    #ax.scatter(a2, 0, s=60, marker=(5, 1))
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(-1.5, 1.5)
+    ax.plot(0, 0, 'k+', ms=10)      # CM
+    ax.plot(a1, 0, 'ko', ms=5)     # M1
+    ax.plot(a2, 0, 'ko', ms=5)     # M2
+
+    CS = ax.contour(
+        X, Y, Z,
+        levels=levels,
+        colors='black',
+        linewidths=0.4
+    )
+
+    ax.text(a1+0.01, -0.01, r'$M_1$', color='k', fontsize=10,
+            va='top', ha='left')
+    ax.text(a2+0.01, -0.01, r'$M_2$', color='k', fontsize=10,
+            va='top', ha='left')
+
+    ax.set_title("Roche equipotentials for q=" + str(q))
+    plt.show()
+
+
+    Zplot = np.clip(Z, np.percentile(Z, 0.8), np.percentile(Z, 100))
+    fig2 = plt.figure(figsize=(10, 7), dpi=300)
+    ax2 = fig2.add_subplot(111, projection='3d')
+
+    ax2.plot_surface(
+        X, Y, Zplot,
+        rstride=100, cstride=100,
+        color='white',
+        edgecolor='k',
+        linewidth=0.3,
+        shade=True
+    )
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('y')
+    ax2.set_zlabel(r'$\Phi_{\rm Roche}$')
+    ax2.set_box_aspect((1, 1, 0.3))
+    # view
+    ax2.view_init(elev=35, azim=-60)
+    ax2.set_title("Roche potential for q=" + str(q))
+
+    plt.show()
+
+
+    #Solve the x of L1 L2 L3
+    roots = []
+    roots += [bisect_root(lambda x: f(x, mu), xmin, mu-1-eps)]
+    roots += [bisect_root(lambda x: f(x, mu), mu-1+eps, mu-eps)]
+    roots += [bisect_root(lambda x: f(x, mu), mu+eps, xmax)]
+
+    #print("Roots (x):", roots)
+
+    xL2 = [r for r in roots if r < mu-1][0]  
+    xL1 = [r for r in roots if mu-1 < r < mu][0] 
+    xL3 = [r for r in roots if r > mu][0]
+
+    fig3, ax3 = plt.subplots(figsize=(10, 6), dpi=300)
+
+    ax3.plot(xs1, f(xs1,mu), 'b', lw=1)
+    ax3.plot(xs2, f(xs2,mu), 'r', lw=1)
+    ax3.plot(xs3, f(xs3,mu), 'g', lw=1)
+
+    ax3.axhline(0, color='gray', lw=0.8)
+    ax3.axvline(mu-1, color='gray', lw=0.8, ls='--')
+    ax3.axvline(mu,   color='gray', lw=0.8, ls='--')
+
+    ax3.set_title(
+        r"$f(x)=x-\frac{1-\mu}{(x-\mu)^2}\operatorname{sgn}(x-\mu)"
+        r"-\frac{\mu}{(x-\mu+1)^2}\operatorname{sgn}(x-\mu+1)$"
+        f",  $q={q}$"
+    )
+    ax3.set_xlabel("x")
+    ax3.set_ylabel("f(x)")
+    ax3.set_ylim(-10, 10)
+
+    ax3.plot(0, 0, 'k+', ms=10)      # CM
+    ax3.plot(a1, 0, 'ko', ms=5)      # M1
+    ax3.plot(a2, 0, 'ko', ms=5)      # M2
+
+    # Lagrange points
+    ax3.plot(xL2, f(xL2, mu), 'b^', ms=5)
+    ax3.plot(xL1, f(xL1, mu), 'r^', ms=5)
+    ax3.plot(xL3, f(xL3, mu), 'g^', ms=5)
+
+    ax3.text(xL2+0.01, f(xL2, mu)-0.3, ' L2', color='b', fontsize=10,
+            va='top', ha='left')
+    ax3.text(xL1+0.01, f(xL1, mu)-0.3, ' L1', color='r', fontsize=10,
+            va='top', ha='left')
+    ax3.text(xL3+0.01, f(xL3, mu)-0.3, ' L3', color='g', fontsize=10,
+            va='top', ha='left')
+
+    ax3.text(-2, -2.5,
+            rf'$x(L_1)={xL1:.8f}$' '\n'
+            rf'$x(L_2)={xL2:.8f}$' '\n'
+            rf'$x(L_3)={xL3:.8f}$',
+            fontsize=10, ha='left', va='top')
+
+    plt.show()
+
+
+
+    xL4,xL5 = mu-1/2, mu-1/2
+    yL4,yL5 = np.sqrt(3)/2, -np.sqrt(3)/2
+    RP_L1 = roche_potential(xL1, 0, q)
+    RP_L2 = roche_potential(xL2, 0, q)
+    RP_L3 = roche_potential(xL3, 0, q)
+    RP_L4 = roche_potential(xL4, yL4, q)
+    RP_L5 = roche_potential(xL5, yL5, q)
+    fig4, ax4 = plt.subplots(figsize=(10, 10), dpi=300)
+    ax4.set_aspect('equal')
+    ax4.set_xticks([])
+    ax4.set_yticks([])
+    ax4.set_frame_on(False)
+
+    ax4.set_xlim(-1.5, 1.5)
+    ax4.set_ylim(-1.5, 1.5)
+
+    RP_Ls = np.array([RP_L1, RP_L2, RP_L3, RP_L4, RP_L5])
+    RP_Ls = np.unique(RP_Ls)
+    RP_Ls.sort()
+
+    CS_base = ax4.contour(
+        X, Y, Z,
+        levels=levels,
+        colors='black',
+        linewidths=0.4
+    )
+    CS_Ls = ax4.contour(
+        X, Y, Z,
+        levels=RP_Ls.tolist(),
+        colors='black',
+        linewidths=2.0
+    )
+
+    Rmask = np.sqrt(X**2 + Y**2)
+
+    mask = Rmask > 1.4
+
+    Z_masked = np.ma.array(Z, mask=mask)
+    CS_L1 = ax4.contour(
+        X, Y, Z_masked,
+        levels=[RP_L1],
+        colors='black',
+        linewidths=4.0
+    )
+
+    ax4.plot(0, 0, 'k+', ms=10)      # CM
+    ax4.plot(a1, 0, 'ko', ms=5)     # M1
+    ax4.plot(a2, 0, 'ko', ms=5)     # M2
+    ax4.text(a1+0.01, -0.01, r'$M_1$', color='k', fontsize=10,
+            va='top', ha='left')
+    ax4.text(a2+0.01, -0.01, r'$M_2$', color='k', fontsize=10,
+            va='top', ha='left')
+
+    ax4.plot(xL1, 0, 'r^', ms=6); ax4.text(xL1+0.03, 0.02, r'$L_1$', fontsize=11, color='r')
+    ax4.plot(xL2, 0, 'r^', ms=6); ax4.text(xL2-0.10, 0.02, r'$L_2$', fontsize=11, color='r')
+    ax4.plot(xL3, 0, 'r^', ms=6); ax4.text(xL3+0.03, 0.02, r'$L_3$', fontsize=11, color='r')
+    ax4.plot(xL4, yL4, 'r^', ms=6); ax4.text(xL4+0.03, yL4+0.03, r'$L_4$', fontsize=11, color='r')
+    ax4.plot(xL5, yL5, 'r^', ms=6); ax4.text(xL5+0.03, yL5-0.08, r'$L_5$', fontsize=11, color='r')
+
+    ax4.set_title(rf"Roche equipotentials with Lagrange points ($q={q}$)")
+    plt.show()
+
+    x5, y5, z5 = 1.1* np.mgrid[-1:1:101j, -1:1:101j, -1:1:101j]
+    vol = roche_potential_3D(x5, y5, z5, q)
+
+    dx5 = x5[1,0,0] - x5[0,0,0]
+    dy5 = y5[0,1,0] - y5[0,0,0]
+    dz5 = z5[0,0,1] - z5[0,0,0]
+
+    iso_val = RP_L1
+    verts, faces, normals, values = measure.marching_cubes(
+        vol, level=iso_val, spacing = (dx5, dy5, dz5)
+    )
+
+    verts_phys = verts.copy()
+    verts_phys[:, 0] += x5.min()
+    verts_phys[:, 1] += y5.min()
+    verts_phys[:, 2] += z5.min()
+
+    roche_lobe_plot(q)
--- main.py
+++ main.py
@@ -5,31 +5,97 @@ import numpy as np
 G = 0.1
 
 # Simulation parameters
-delta = 0.01
-epsilon = 1
+dbg = True # Optional output info for debug
+delta = 0.01 # Grid step
+vector_field_delta = 0.5 # Should be smaller than delta
+epsilon = 1 # Used for regularization near singularities
+diff_step = 0.01 # Used for differentiation
+error = 1e-6 # When an iterative method won't increase the accuracy of the result more than error, it will stop
 size = 8.0
 
 # Physical parameters
 M = 10
 q = 2
-a1 = 2
+a = 4
 Omega = 0.1
 
 # Constraint physical parameters
 M1 = M/(1 + q)
 M2 = M - M1
+a1 = (q * a)/(q + 1)
 a2 = - a1/q
 
+# Compute the Roche potential on the 2D plane containing stars
 def roche_potential(X, Y):
     R = np.sqrt(X**2 + Y**2)
-    R1 = np.sqrt((X - a1)**2 + Y**2)
-    R2 = np.sqrt((X - a2)**2 + Y**2)
-    # Adds epsilon to avoid the singularity at the star position
-    Phi1 = - (G * M1)/(R1 + epsilon)
-    Phi2 = - (G * M2)/(R2 + epsilon)
+    # Adds epsilon to avoid the singularity at the star position and keep the C1 behavior
+    R1 = np.sqrt((X - a1)**2 + Y**2 + epsilon**2)
+    R2 = np.sqrt((X - a2)**2 + Y**2 + epsilon**2)
+    Phi1 = - (G * M1)/(R1)
+    Phi2 = - (G * M2)/(R2)
     PhiC = (1/2) * (Omega**2) * (R**2)
     PhiRoche = Phi1 + Phi2 - PhiC
     return PhiRoche
+
+# Returns the 2D gradient field of the Roche potential in the plane containing the stars
+def grad_roche_potential(X, Y):
+    # R1 = np.sqrt((X - a1)**2 + Y**2 + epsilon**2)
+    # R2 = np.sqrt((X - a2)**2 + Y**2 + epsilon**2)
+    # Diff_X = - (G * M1 * (X - a1))/np.pow(R1, 3) - (G * M2 * (X - a2))/np.pow(R2, 3) + (Omega**2) * X
+    # Diff_Y = - (G * M1 * Y)/np.pow(R1, 3) - (G * M2 * Y)/np.pow(R2, 3) + (Omega**2) * Y
+    Partial_Y, Partial_X = np.gradient(roche_potential(X, Y)) # The order of gradient components returned is a bit confusing, be careful
+    return Partial_X, Partial_Y
+
+# Returns the 2D gradient of the Roche potential at a given point
+def grad_roche(x, y):
+    r1 = np.sqrt((x - a1)**2 + y**2 + epsilon**2)
+    r2 = np.sqrt((x - a2)**2 + y**2 + epsilon**2)
+    return (- (G * M1 * (x - a1))/pow(r1, 3) - (G * M2 * (x - a2))/pow(r2, 3) + (Omega**2) * x), (- (G * M1 * y)/pow(r1, 3) - (G * M2 * y)/pow(r2, 3) + (Omega**2) * y)
+
+def grad_roche_x(x, y):
+    x, _ = grad_roche(x, y)
+    return x
+
+def grad_roche_y(x, y):
+    _, y = grad_roche(x, y)
+    return y
+
+def partial_x_grad_roche_x(x, y):
+    return (grad_roche_x(x + diff_step, y) - grad_roche_x(x - diff_step, y))/(2 * diff_step)
+
+def partial_y_grad_roche_x(x, y):
+    return (grad_roche_x(x, y + diff_step) - grad_roche_x(x, y - diff_step))/(2 * diff_step)
+
+def partial_x_grad_roche_y(x, y):
+    return (grad_roche_y(x + diff_step, y) - grad_roche_y(x - diff_step, y))/(2 * diff_step)
+
+def partial_y_grad_roche_y(x, y):
+    return (grad_roche_y(x, y + diff_step) - grad_roche_y(x, y - diff_step))/(2 * diff_step)
+
+def jacobian_grad_roche(x, y):
+    return np.array([[partial_x_grad_roche_x(x, y), partial_y_grad_roche_x(x, y)], [partial_x_grad_roche_y(x, y), partial_y_grad_roche_y(x, y)]])
+
+point_buffer = []
+def newton_method(x0, y0, step):
+    point_buffer.append((x0, y0))
+    if dbg: print("x = " + str(x0) + " y = " + str(y0))
+    if step == 0:
+        return x0, y0
+    
+    X = np.array([[x0], [y0]])
+    F = np.array([[grad_roche_x(x0, y0)], [grad_roche_y(x0, y0)]])
+    J = jacobian_grad_roche(x0, y0)
+    J_inv = np.linalg.inv(J)
+    X_new = X - J_inv * F
+    x_new = float(X_new[0][0])
+    y_new = float(X_new[1][0])
+
+    if abs(x0 - x_new) < error and abs(y0 - y_new) < error:
+        if dbg: print("Return earlier")
+        return x_new, y_new
+
+    return newton_method(x_new, y_new, step-1)
+
 
 if __name__ == "__main__":
     x = np.arange(-size, size, delta)
@@ -39,8 +105,32 @@ if __name__ == "__main__":
     Z = roche_potential(X, Y)
     fig, ax = plt.subplots()
     ax.scatter(0, 0, s=60, marker=".")
+
+    x_L1, y_L1 = newton_method(0, 0, 10)
+    x_L2, y_L2 = newton_method(2 * a2, 0, 10)
+    x_L3, y_L3 = newton_method(2 * a1, 0, 10)
+
+    print("x_L1 = " + str(x_L1) + " y_L1 = " + str(y_L1))
+    print("x_L2 = " + str(x_L2) + " y_L2 = " + str(y_L2))
+    print("x_L3 = " + str(x_L3) + " y_L3 = " + str(y_L3))
+
+    x_L4, y_L4 = newton_method(0, a, 100)
+
+    for xp, yp in point_buffer:
+        ax.scatter(xp, yp, s=40, marker="+", c=[(1, 0, 0)])
+
     ax.scatter(a1, 0, s=60, marker=(5, 1))
     ax.scatter(a2, 0, s=60, marker=(5, 1))
-    CS = ax.contour(X, Y, Z, 20)
+    CS = ax.contour(X, Y, Z, 10)
+
+    u = np.arange(-size, size, vector_field_delta)
+    v = np.arange(-size, size, vector_field_delta)
+    U, V = np.meshgrid(u, v)
+    Partial_U, Partial_V = grad_roche_potential(U, V)
+    quiver = ax.quiver(U, V, Partial_U, Partial_V)
+
     ax.set_title("Roche equipotentials for M=" + str(M) + ", q=" + str(q))
+
     plt.show()
+
+    
